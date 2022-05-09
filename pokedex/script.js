83982912e76bbe9422a16dae9560960daa5dcc91
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-05-09 11:16:49 -0500

    Now fetching pokemons

--- pokedex/script.js
+++ pokedex/script.js
@@ -152,113 +152,6 @@ const pokemonColorMap = {
   151: "#ffb6c3",
 };
 
-const dataPokemons = {
-  count: 1118,
-  next: "https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20",
-  previous: null,
-  results: [
-    {
-      name: "bulbasaur",
-      url: "https://pokeapi.co/api/v2/pokemon/1/",
-    },
-    {
-      name: "ivysaur",
-      url: "https://pokeapi.co/api/v2/pokemon/2/",
-    },
-    {
-      name: "venusaur",
-      url: "https://pokeapi.co/api/v2/pokemon/3/",
-    },
-    {
-      name: "charmander",
-      url: "https://pokeapi.co/api/v2/pokemon/4/",
-    },
-    {
-      name: "charmeleon",
-      url: "https://pokeapi.co/api/v2/pokemon/5/",
-    },
-    {
-      name: "charizard",
-      url: "https://pokeapi.co/api/v2/pokemon/6/",
-    },
-    {
-      name: "squirtle",
-      url: "https://pokeapi.co/api/v2/pokemon/7/",
-    },
-    {
-      name: "wartortle",
-      url: "https://pokeapi.co/api/v2/pokemon/8/",
-    },
-    {
-      name: "blastoise",
-      url: "https://pokeapi.co/api/v2/pokemon/9/",
-    },
-    {
-      name: "caterpie",
-      url: "https://pokeapi.co/api/v2/pokemon/10/",
-    },
-    {
-      name: "metapod",
-      url: "https://pokeapi.co/api/v2/pokemon/11/",
-    },
-    {
-      name: "butterfree",
-      url: "https://pokeapi.co/api/v2/pokemon/12/",
-    },
-    {
-      name: "weedle",
-      url: "https://pokeapi.co/api/v2/pokemon/13/",
-    },
-    {
-      name: "kakuna",
-      url: "https://pokeapi.co/api/v2/pokemon/14/",
-    },
-    {
-      name: "beedrill",
-      url: "https://pokeapi.co/api/v2/pokemon/15/",
-    },
-    {
-      name: "pidgey",
-      url: "https://pokeapi.co/api/v2/pokemon/16/",
-    },
-    {
-      name: "pidgeotto",
-      url: "https://pokeapi.co/api/v2/pokemon/17/",
-    },
-    {
-      name: "pidgeot",
-      url: "https://pokeapi.co/api/v2/pokemon/18/",
-    },
-    {
-      name: "rattata",
-      url: "https://pokeapi.co/api/v2/pokemon/19/",
-    },
-    {
-      name: "raticate",
-      url: "https://pokeapi.co/api/v2/pokemon/20/",
-    },
-    {
-      name: "spearow",
-      url: "https://pokeapi.co/api/v2/pokemon/21/",
-    },
-    {
-      name: "fearow",
-      url: "https://pokeapi.co/api/v2/pokemon/22/",
-    },
-    {
-      name: "ekans",
-      url: "https://pokeapi.co/api/v2/pokemon/23/",
-    },
-    {
-      name: "arbok",
-      url: "https://pokeapi.co/api/v2/pokemon/24/",
-    },
-    {
-      name: "pikachu",
-      url: "https://pokeapi.co/api/v2/pokemon/25/",
-    },
-  ],
-};
 
 function getPokemonImageUri(id) {
   const imageId = ("00" + id).slice(-3); // para 1 => 001
@@ -289,26 +182,21 @@ class Pokedex extends HTMLElement {
   }
 }
 
-// const test = new Promise((resolve, reject) => {
-//   return resolve(
-//     fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20").then(
-//       (response) => response.json()
-//     )
-//   );
-// });
-
-// const responseApi = Promise.resolve.then();
-// console.log(test);
-dataPokemons.results.forEach((pokemon, index) => {
-  let color = pokemonColorMap[index + 1];
-  let pokeElement = document.createElement("poke-card");
-  pokeElement.setAttribute("image", getPokemonImageUri(index + 1));
-  pokeElement.setAttribute("name", pokemon.name);
-  pokeElement.setAttribute("color", color);
-  pokeElement.setAttribute(
-    "style",
-    `color: ${color.split("#")[1].startsWith("f") ? "black" : "white"}`
-  );
-  document.getElementById("pokedex").appendChild(pokeElement);
-});
-customElements.define("poke-card", Pokedex);
+fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20')
+.then(async res => {
+  const data = await res.json()
+  const pokemons = data.results
+  pokemons.forEach((pokemon, index) => {
+    let color = pokemonColorMap[index + 21];
+    let pokeElement = document.createElement("poke-card");
+    pokeElement.setAttribute("image", getPokemonImageUri(index + 21));
+    pokeElement.setAttribute("name", pokemon.name);
+    pokeElement.setAttribute("color", color);
+    pokeElement.setAttribute(
+      "style",
+      `color: ${color.split("#")[1].startsWith("f") ? "black" : "white"}`
+    );
+    document.getElementById("pokedex").appendChild(pokeElement);
+  })
+  customElements.define("poke-card", Pokedex);
+})
