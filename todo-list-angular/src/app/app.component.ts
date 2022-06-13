3c60530446ceeb897c1e60c41d6c5089db537885
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-06-13 17:02:58 -0500

    Basic to do list using angular

---
+++ todo-list-angular/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input } from '@angular/core';
+
+interface Item {
+  name: string;
+  done: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo-list-angular';
+  tasks: Item[] = [];
+  @Input()
+  item: Item = { name: '', done: false };
+
+  addItem(task: string) {
+    this.tasks.unshift({
+      name: task,
+      done: false
+    });
+  }
+}
