ba3789ec97283fb269b8e42cbe7c52921e9e6489
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-06-23 14:47:15 -0500

    Challenge 1 - Added more styles

--- pokedex-angular/src/app/pokedex-list/pokedex-list.component.ts
+++ pokedex-angular/src/app/pokedex-list/pokedex-list.component.ts
@@ -1,24 +1,24 @@
-import { Component, OnInit } from "@angular/core";
-
-type Pokemon = {
-    name: string;
-    url: string;
-}
+import { Component, OnInit } from '@angular/core';
+import { Pokemon, pokedex, pokemonColorMap } from 'src/assets/utils';
 
 @Component({
-    selector: 'app-pokedex-list',
-    templateUrl: './podekex-list.component.html',
-    styleUrls: ['./pokedex-list.component.scss']
+  selector: 'app-pokedex-list',
+  templateUrl: './podekex-list.component.html',
+  styleUrls: ['./pokedex-list.component.scss'],
 })
 export class PokedexListComponent implements OnInit {
-    pokedex: Pokemon[] = [{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" }, { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" }, { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" }, { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" }, { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" }, { name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" }, { name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" }, { name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/" }, { name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/" }, { name: "caterpie", url: "https://pokeapi.co/api/v2/pokemon/10/" }, { name: "metapod", url: "https://pokeapi.co/api/v2/pokemon/11/" }, { name: "butterfree", url: "https://pokeapi.co/api/v2/pokemon/12/" }, { name: "weedle", url: "https://pokeapi.co/api/v2/pokemon/13/" }, { name: "kakuna", url: "https://pokeapi.co/api/v2/pokemon/14/" }, { name: "beedrill", url: "https://pokeapi.co/api/v2/pokemon/15/" }, { name: "pidgey", url: "https://pokeapi.co/api/v2/pokemon/16/" }, { name: "pidgeotto", url: "https://pokeapi.co/api/v2/pokemon/17/" }, { name: "pidgeot", url: "https://pokeapi.co/api/v2/pokemon/18/" }, { name: "rattata", url: "https://pokeapi.co/api/v2/pokemon/19/" }, { name: "raticate", url: "https://pokeapi.co/api/v2/pokemon/20/" }, { name: "spearow", url: "https://pokeapi.co/api/v2/pokemon/21/" }, { name: "fearow", url: "https://pokeapi.co/api/v2/pokemon/22/" }, { name: "ekans", url: "https://pokeapi.co/api/v2/pokemon/23/" }, { name: "arbok", url: "https://pokeapi.co/api/v2/pokemon/24/" }, { name: "pikachu", url: "https://pokeapi.co/api/v2/pokemon/25/" }, { name: "raichu", url: "https://pokeapi.co/api/v2/pokemon/26/" }, { name: "sandshrew", url: "https://pokeapi.co/api/v2/pokemon/27/" }, { name: "sandslash", url: "https://pokeapi.co/api/v2/pokemon/28/" }, { name: "nidoran-f", url: "https://pokeapi.co/api/v2/pokemon/29/" }, { name: "nidorina", url: "https://pokeapi.co/api/v2/pokemon/30/" }, { name: "nidoqueen", url: "https://pokeapi.co/api/v2/pokemon/31/" }, { name: "nidoran-m", url: "https://pokeapi.co/api/v2/pokemon/32/" }, { name: "nidorino", url: "https://pokeapi.co/api/v2/pokemon/33/" }, { name: "nidoking", url: "https://pokeapi.co/api/v2/pokemon/34/" }, { name: "clefairy", url: "https://pokeapi.co/api/v2/pokemon/35/" }, { name: "clefable", url: "https://pokeapi.co/api/v2/pokemon/36/" }, { name: "vulpix", url: "https://pokeapi.co/api/v2/pokemon/37/" }, { name: "ninetales", url: "https://pokeapi.co/api/v2/pokemon/38/" }, { name: "jigglypuff", url: "https://pokeapi.co/api/v2/pokemon/39/" }, { name: "wigglytuff", url: "https://pokeapi.co/api/v2/pokemon/40/" }];
+  myPokedex: Pokemon[] = pokedex;
 
-    ngOnInit(): void {
-        this.pokedex.map((pokemon, index) => pokemon.url = this.getPokemonImageUri(index + 1))
-    }
+  ngOnInit(): void {
+    this.myPokedex.map((pokemon, index) => {
+      const realIndex: number = index + 1;
+      pokemon.url = this.getPokemonImageUri(realIndex);
+      pokemon.color = pokemonColorMap[realIndex];
+    });
+  }
 
-    getPokemonImageUri(id: number) {
-        const imageId = ('00' + id).slice(-3); // para 1 => 001
-        return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${imageId}.png`;
-    }
+  getPokemonImageUri(id: number) {
+    const imageId = ('00' + id).slice(-3); // para 1 => 001
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${imageId}.png`;
+  }
 }
