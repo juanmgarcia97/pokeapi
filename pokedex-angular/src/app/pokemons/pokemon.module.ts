c055b3c34a8504285d6eb7857100cb489ced6612
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-06-30 16:50:39 -0500

    update pokemon module

--- pokedex-angular/src/app/pokemons/pokemon.module.ts
+++ pokedex-angular/src/app/pokemons/pokemon.module.ts
@@ -4,15 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { PokedexListComponent } from './pokedex-list/pokedex-list.component';
 import { PokemonFilterComponent } from './pokemon-filter/pokemon-filter.component';
+import { PokemonRoutingModule } from './pokemon-routing.module';
 import { PokeCardComponent } from './pokemon/poke-card.component';
+import { PokemonProfileComponent } from './profile/pokemon-profile.component';
 
 @NgModule({
   declarations: [
     PokedexListComponent,
     PokeCardComponent,
     PokemonFilterComponent,
+    PokemonProfileComponent,
   ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  exports: [PokedexListComponent, PokeCardComponent],
+  exports: [PokedexListComponent, PokeCardComponent, PokemonRoutingModule],
 })
 export class PokemonModule {}
