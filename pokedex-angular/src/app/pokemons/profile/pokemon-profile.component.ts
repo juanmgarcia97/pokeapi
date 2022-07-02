785ba46d58d5726b94d75575bcd6be47bc5b3989
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-07-02 08:52:27 -0500

    showing pokemon info on profile component

--- pokedex-angular/src/app/pokemons/profile/pokemon-profile.component.ts
+++ pokedex-angular/src/app/pokemons/profile/pokemon-profile.component.ts
@@ -1,13 +1,23 @@
 import { Location } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon, PokemonApi } from 'src/app/utils/types';
 
 @Component({
   selector: 'app-pokemon-profile',
   templateUrl: './pokemon-profile.component.html',
   // styleUrls: ['./pokemon-profile.component.scss'],
 })
-export class PokemonProfileComponent {
-  constructor(private location: Location) {}
+export class PokemonProfileComponent implements OnInit {
+  pokemon!: Pokemon;
+  pokemonProfile!: PokemonApi;
+  constructor(private location: Location, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.url.subscribe(
+      (data) => (this.pokemon = data[1].parameters as unknown as Pokemon)
+    );
+  }
 
   goBack() {
     this.location.back();
