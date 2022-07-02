69731c63475ad6a86815fb73a725d9489f04b16a
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-07-02 08:52:57 -0500

    added pokemon api type and updated old one

--- pokedex-angular/src/app/utils/types.ts
+++ pokedex-angular/src/app/utils/types.ts
@@ -2,10 +2,27 @@ export type Pokemon = {
   id?: number;
   name: string;
   url: string;
+  image: string;
   color: string;
 };
 
 export type PokemonApi = {
+  abilities: unknown[];
+  base_experience: number;
+  forms: unknown[];
+  game_indices: unknown[];
+  height: number;
+  id: number;
+  held_items: unknown[];
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: unknown[];
   name: string;
-  url: string;
+  order: number;
+  past_types: unknown[];
+  species: unknown;
+  sprites: unknown;
+  stats: unknown[];
+  types: unknown[];
+  weight: number;
 };
