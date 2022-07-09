09d917975cfc5cdcf8a2a3604423d6a654ebfd47
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-07-09 14:16:26 -0500

    added some types

--- pokedex-angular/src/app/utils/types.ts
+++ pokedex-angular/src/app/utils/types.ts
@@ -1,5 +1,5 @@
 export type Pokemon = {
-  id?: number;
+  id: number;
   name: string;
   url: string;
   image: string;
@@ -22,7 +22,51 @@ export type PokemonApi = {
   past_types: any[];
   species: any;
   sprites: any;
-  stats: any[];
+  stats: Stat[];
   types: any[];
   weight: number;
+};
+
+export type Stat = {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+};
+
+export type PokemonSpecies = {
+  base_happiness: any;
+  capture_rate: any;
+  color: {};
+  egg_groups: [];
+  evolution_chain: any;
+  evolves_from_species: any;
+  flavor_text_entries: FlavorText[];
+  form_descriptions: [];
+  forms_switchable: boolean;
+  gender_rate: number;
+  genera: [];
+  generation: {};
+  growth_rate: {};
+  habitat: any;
+  has_gender_differences: boolean;
+  hatch_counter: any;
+  id: number;
+  is_baby: boolean;
+  is_legendary: boolean;
+  is_mythical: boolean;
+  name: string;
+  order: number;
+  pal_park_encounters: [];
+  pokedex_numbers: [];
+  shape: {};
+  varieties: [];
+};
+
+export type FlavorText = {
+  flavor_text: string;
+  language: {};
+  version: {};
 };
