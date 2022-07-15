1ed7f332f1b935f3bb5ec26e424277b6675e66fb
Juan Martin Garcia <martin.garcia@iterrabyte.com>
2022-07-14 23:59:07 -0500

    Added select generations but is not working well

--- pokedex-angular/src/app/pokemons/pokedex-list/pokedex-list.component.ts
+++ pokedex-angular/src/app/pokemons/pokedex-list/pokedex-list.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { pokemonColorMap } from 'src/app/utils/mock-data';
-import { Pokemon } from 'src/app/utils/types';
+import { Pokemon, PokemonApi } from 'src/app/utils/types';
 import { PokemonService } from '../pokemon.service';
+import { generations } from './pokedex-list.generations';
 
 @Component({
   selector: 'app-pokedex-list',
@@ -10,12 +12,14 @@ import { PokemonService } from '../pokemon.service';
   styleUrls: ['./pokedex-list.component.scss'],
 })
 export class PokedexListComponent implements OnInit {
-  myPokedex: Pokemon[] = [];
-  displayPokedex: Pokemon[] = [];
+  myPokedex: PokemonApi[] = [];
+  displayPokedex: PokemonApi[] = [];
   limit: number = 50;
   offset: number = 0;
   @Input()
   search = '';
+  pokemonGenerations = generations;
+  generationSelected = '0';
 
   constructor(
     private pokemonService: PokemonService,
@@ -26,6 +30,17 @@ export class PokedexListComponent implements OnInit {
     this.myPokedex = this.router.snapshot.data['pokemons'];
     this.displayPokedex = this.myPokedex;
     this.offset += this.limit;
+  }
+
+  getPokemonGeneration() {
+    // this.pokemonService
+    //   .getPokemonsByGeneration(this.generationSelected)
+    //   .subscribe((pokemons) => {
+    //     console.log(pokemons.slice(0, 50));
+
+    //     this.myPokedex = [...this.myPokedex, ...pokemons];
+    //     this.displayPokedex = this.myPokedex;
+    //   });
   }
 
   paginator() {
---
+++ pokedex-angular/src/app/pokemons/pokedex-list/pokedex-list.generations.ts
@@ -0,0 +1,18 @@
+export const generations = [
+  {
+    name: 'Generation 1',
+    value: 1
+  },
+  {
+    name: 'Generation 2',
+    value: 2
+  },
+  {
+    name: 'Generation 3',
+    value: 3
+  },
+  {
+    name: 'Generation 4',
+    value: 4
+  }
+]
