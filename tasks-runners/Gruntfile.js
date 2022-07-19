fd6b332d92e2542847e44cfe82285df1551c63e1
Juan Martín García <juanmar-p@hotmail.com>
2022-07-19 13:23:28 -0500

    Deleted other projects

--- tasks-runners/Gruntfile.js
+++
@@ -1,2 +0,0 @@
-if (process.env.npm_execpath.match('yarn'))
-  throw new Error('Use NPM instead of Yarn');
--- typescript/poke-ts/src/main.ts
+++
@@ -1,4 +0,0 @@
-import { PokemonTrainer } from './Pokemon';
-
-const pokemonMaster = new PokemonTrainer('Misty');
-pokemonMaster.showTeam();
