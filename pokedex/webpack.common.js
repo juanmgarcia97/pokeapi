ae00da3f67b2bdbe7f673309c73c5d5dacb98394
Juan Martin Garcia <juanmar-p@hotmail.com>
2022-07-18 21:10:47 -0500

    dist loaded to gh-pages

--- pokedex/webpack.common.js
+++
@@ -1,8 +0,0 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: {
-    script: './src/script.js',
-  },
-  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
-};
--- typescript/poke-ts/src/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pokemon_1 = require("./Pokemon");
-var pokemonMaster = new Pokemon_1.PokemonTrainer('Misty');
-pokemonMaster.showTeam();
