093cc2160026c1fd010422a4646881ca54e7ad7e
Juan Martin Garcia <juanmar-p@hotmail.com>
2022-07-18 20:48:32 -0500

    app refactor

--- pokedex-angular/src/app/core/core.module.ts
+++ pokedex-angular/src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [CommonModule, FormsModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}
