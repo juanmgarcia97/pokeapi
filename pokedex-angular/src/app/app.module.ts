b3693e3aaedcd178457d43c75945cfd16c808c6b
Juan Martin Garcia <juanmar-p@hotmail.com>
2022-07-18 20:46:36 -0500

    pwa generated code

--- pokedex-angular/src/app/app.module.ts
+++ pokedex-angular/src/app/app.module.ts
@@ -5,10 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './core/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, HttpClientModule, CoreModule],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule, CoreModule, BrowserAnimationsModule, FontAwesomeModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
